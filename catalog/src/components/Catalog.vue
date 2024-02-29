d7ae29f4a96ae3dab9268bc6581c605919ba1fee
Polya Tomson <polyatomson@gmail.com>
2024-02-29 19:16:24 +0100

    found an acceptable scaling solution

--- catalog/src/components/Catalog.vue
+++ catalog/src/components/Catalog.vue
@@ -2,32 +2,32 @@
     <div class="flex justify-content-center">
         <Button label="Reload" @click="getCatalog"/>
     </div>
-    <Card style="margin: auto; margin-top: 1rem;" class="pd-card" :pt="{root: {style: tableWidth}, header: {class: 'bg-primary'}, content: {class: 'text-sm'}}">
+    <Card style="margin: auto; margin-top: 1rem;" class="pd-card" :pt="{header: {class: 'bg-primary'}, content: {class: 'text-sm'}}">
         <template #header>
             <div style="height: 1rem;"></div>
         </template>
         <template #content>
-            <DataTable :value="songs" size="small" :pt="{wrapper: {style: tableWidth}, virtualScroller:{autoSize: true}, table: {style: tableWidth}}" scrollable scrollHeight="300rem">
-                <Column field="origin" style="max-width: 10%;" header="Origin">
+            <DataTable :value="songs" size="small" scrollable scrollHeight="300rem">
+                <Column v-if="!mobile" field="origin" style="max-width: 10rem;" header="Origin">
                     <template #body="{ data, field }">
                         <Tag :value="data[field]" :severity="getColor(data[field])" />
                     </template>
                 </Column>
-                <Column field="artist" style="max-width: 20%;" :pt="{body: {class: 'flex flex-wrap'}}" header="Artist"></Column>
-                <Column field="title" style="max-width: 35%;" :pt="{body: {class: 'flex flex-wrap'}}" header="Title"></Column>
-                <Column field="link" style="max-width: 10%;" header="Link">
+                <Column field="artist" style="max-width: 8rem;" :pt="{body: {class: 'flex flex-wrap'}}" header="Artist"></Column>
+                <Column field="title" style="max-width: 8rem" :pt="{body: {class: 'flex flex-wrap'}}" header="Title"></Column>
+                <Column v-if="!mobile" field="vocals" style="max-width: 10%" header="Voice">
+                    <template #body="{ data, field}">
+                        <i v-if="data[field]===1" class="pi pi-check" style="font-size: 1rem"></i>
+                        <i v-if="data[field]===0" class="pi pi-times" style="font-size: 1rem"></i>
+                        </template>
+                </Column>
+                <Column field="link" style="max-width: 10rem;" frozen alignFrozen="right" header="Link">
                 <template #body="{ data, field }">
                 <a :href="data[field]" target="_blank" rel="noopener noreferrer">
                     <Button v-if="!viewed.includes(data.song_id)" label="Open" class="text-sm" size="small" severity="info" @click="addView(data.song_id)"/>
                     <Button v-else="viewed.includes(data.song_id)" label="Repeat" class="text-sm" size="small" severity="warning" @click="addView(data.song_id)"/>
                 </a>
                 </template>
-                </Column>
-                <Column field="vocals" style="max-width: 10%" header="Voice">
-                    <template #body="{ data, field}">
-                        <i v-if="data[field]===1" class="pi pi-check" style="font-size: 1rem"></i>
-                        <i v-if="data[field]===0" class="pi pi-times" style="font-size: 1rem"></i>
-                        </template>
                 </Column>
             </DataTable>
         </template>
@@ -37,7 +37,7 @@
 import { onMounted, ref } from 'vue'
 const songs = ref()
 const viewed = ref([])
-const tableWidth = ref()
+const mobile = ref()
 
 const getColor = (origin) => {
     switch (origin) {
@@ -94,15 +94,11 @@ async function addView(song_id) {
 const detectMob = () => {
     return ( ( window.outerWidth <= 500 ) );
   }
+
 onMounted(async () => {
     if (detectMob()) {
-        tableWidth.value = "width:" + window.outerWidth.toString() + "px;"
+        mobile.value = true
     }
-    else {
-        tableWidth.value = "width: 100%;"
-    }
-    console.log('table width', tableWidth.value)
-    // console.log(window.outerWidth)
     getCatalog()
 }
 )
