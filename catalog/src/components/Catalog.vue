d5efac39218b107301c0651e531f581dd9bb477a
Polya Tomson <polyatomson@gmail.com>
2024-02-29 16:30:39 +0100

    minified for phone

--- catalog/src/components/Catalog.vue
+++ catalog/src/components/Catalog.vue
@@ -2,31 +2,32 @@
     <div class="flex justify-content-center">
         <Button label="Reload" @click="getCatalog"/>
     </div>
-    <Card style="margin: auto; margin-top: 1rem;" class="pd-card" :pt="{header: {class: 'bg-primary'}}">
+    <Card style="margin: auto; margin-top: 1rem;" class="pd-card" :pt="{header: {class: 'bg-primary'}, content: {class: 'text-sm'}}">
         <template #header>
             <div style="height: 1rem;"></div>
         </template>
         <template #content>
-            <DataTable :value="songs">
+            <DataTable :value="songs" size="small" scrollable scrollHeight="300rem">
                 <Column field="origin" header="Origin">
                     <template #body="{ data, field }">
                         <Tag :value="data[field]" :severity="getColor(data[field])" />
                     </template>
                 </Column>
-                <Column field="artist" header="Artist"></Column>
-                <Column field="title" header="Title"></Column>
+                <Column field="artist" style="max-width: 5rem;" :pt="{body: {class: 'flex flex-wrap'}}" header="Artist"></Column>
+                <Column field="title" style="max-width: 5rem;" :pt="{body: {class: 'flex flex-wrap'}}" header="Title"></Column>
+                <Column field="link" header="Link">
+                <template #body="{ data, field }">
+                <a :href="data[field]" target="_blank">
+                    <Button v-if="!viewed.includes(data.song_id)" label="Open" class="text-sm" size="small" severity="info" @click="addView(data.song_id)"/>
+                    <Button v-else="viewed.includes(data.song_id)" label="Repeat" class="text-sm" size="small" severity="warning" @click="addView(data.song_id)"/>
+                </a>
+                </template>
+                </Column>
                 <Column field="vocals" header="Voice">
                     <template #body="{ data, field}">
                         <i v-if="data[field]===1" class="pi pi-check" style="font-size: 1rem"></i>
                         <i v-if="data[field]===0" class="pi pi-times" style="font-size: 1rem"></i>
                         </template>
-                </Column>
-                <Column field="link" header="Link">
-                <template #body="{ data, field }">
-                <a :href="data[field]" target="_blank">
-                    <Button label="Open" severity="info" @click="addView(data.song_id)"/>
-                </a>
-                </template>
                 </Column>
             </DataTable>
         </template>
@@ -34,7 +35,8 @@
 </template>
 <script setup>
 import { onMounted, ref } from 'vue'
-const songs = ref() 
+const songs = ref()
+const viewed = ref([])
 
 const getColor = (origin) => {
     switch (origin) {
@@ -81,8 +83,10 @@ async function addView(song_id) {
             },
             body: JSON.stringify({'song_id': song_id})
           })
-    reply = await response.text();
+    const reply = await response.text();
     console.log(reply)
+
+    viewed.value.push(song_id)
     
 }
 
