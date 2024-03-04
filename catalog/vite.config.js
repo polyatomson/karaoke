441297cadebd4d6bcdfcba82c4a60e914b9b89a3
Polya Tomson <polyatomson@gmail.com>
2024-03-04 12:11:01 +0100

    build works

--- catalog/vite.config.js
+++ catalog/vite.config.js
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+process.env = {...process.env, ...loadEnv('production', process.cwd())};
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.VITE_APP_BASE,
+  base: process.env.VITE_APP_BASE,
   plugins: [
     vue(),
   ],
